5dec0be2e528282de6ab3a60d418129e92607310
Paul Draper <paul@lucidchart.com>
2017-03-06 00:48:01 -0700

    runAndRestore methods

--- thread-context/src/main/java/com/github/threadcontext/Saver.java
+++ thread-context/src/main/java/com/github/threadcontext/Saver.java
@@ -1,5 +1,8 @@
 package com.github.threadcontext;
 
+import java.util.concurrent.Callable;
+import java.util.function.Supplier;
+
 public interface Saver {
 
     Runnable save();
@@ -8,6 +11,24 @@ public interface Saver {
         Runnable restore = save();
         try {
             runnable.run();
+        } finally {
+            restore.run();
+        }
+    }
+
+    default <T> T runAndRestore(Callable<T> callable) throws Exception {
+        Runnable restore = save();
+        try {
+            return callable.call();
+        } finally {
+            restore.run();
+        }
+    }
+
+    default <T> T runAndRetore(Supplier<T> supplier) {
+        Runnable restore = save();
+        try {
+            return supplier.get();
         } finally {
             restore.run();
         }
