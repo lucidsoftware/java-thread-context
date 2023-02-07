1c8284a30d70a16de3625cd0b9811cc664293878
Sriraam AS <sriraam@lucidchart.com>
2023-02-07 15:01:08 -0500

    use iterator instead of for loop to avoid concurrent modification

--- thread-context/src/main/java/com/github/threadcontext/CombinedContextSupplier.java
+++ thread-context/src/main/java/com/github/threadcontext/CombinedContextSupplier.java
@@ -1,6 +1,7 @@
 package com.github.threadcontext;
 
 import java.util.function.Supplier;
+import java.util.Iterator;
 
 public final class CombinedContextSupplier implements Supplier<Context> {
 
@@ -12,7 +13,9 @@ public final class CombinedContextSupplier implements Supplier<Context> {
 
     public Context get() {
         Context result = null;
-        for (Supplier<Context> supplier : suppliers) {
+        Iterator<Supplier<Context>> iterator = this.suppliers.iterator();
+        while (iterator.hasNext()) {
+            Supplier<Context> supplier = iterator.next();
             result = result == null ? supplier.get() : result.andThen(supplier.get());
         }
         return result == null ? NullContext.INSTANCE : result;
